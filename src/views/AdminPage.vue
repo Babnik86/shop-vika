e1a57e16dc2678ef7d54232c31fec448421b38b2
Oleg Babiy <oleg.babij@idesigni.co.uk>
2018-01-14 19:50:23 +0200

    admin route

---
+++ src/views/AdminPage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        Cart
+        <pre>{{productList}}</pre>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'admin-page',
+        data(){
+            return {
+                productList: [],
+            }
+        },
+        methods: {
+        },
+        created() {
+            let queryBuilder = Backendless.DataQueryBuilder.create().setWhereClause( `user.objectId = '0C957E39-43A1-9243-FF8F-AFDD531AE200'` );
+
+            Backendless.Data.of( "Cart" ).find( queryBuilder )
+                .then( ( cart ) => {
+                    this.productList = cart[0].products
+                    console.log(this.productList)
+                })
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
