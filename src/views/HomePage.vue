01fc6726af1598d75d760ce0cd12129cb6b1e365
Oleg Babiy <oleg.babij@idesigni.co.uk>
2018-01-15 13:23:56 +0200

    refactor productlist

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,12 +2,22 @@
     <div>
         <v-text-field label="Поиск" v-model="search" required></v-text-field>
         <v-btn @click="goSearch"><v-icon> search </v-icon></v-btn>
-        <product-list :product-list="productList"></product-list>
+        <div v-if="productList[0]">
+            <h1>{{productList[0].cat.cat_name}}</h1>
+            <v-container
+                    fluid
+                    style="min-height: 0;"
+                    grid-list-lg>
+                <v-layout row wrap>
+                    <product-card v-for="product in productList" :product="product" :key="product.objectId"></product-card>
+                </v-layout>
+            </v-container>
+        </div>
     </div>
 </template>
 
 <script>
-    import ProductList from './../components/ProductList'
+    import ProductCard from './../components/ProductCard'
 
     export default {
         name: 'home-page',
@@ -34,7 +44,7 @@
                 })
 
         },
-        components: { ProductList }
+        components: { ProductCard }
     }
 </script>
 
