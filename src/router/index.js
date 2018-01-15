78c7135e84755ddefda1c6ac2b25ea5452ddb4f5
Oleg Babiy <oleg.babij@idesigni.co.uk>
2018-01-15 12:24:05 +0200

    admin pag

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueLocalStorage from 'vue-ls';
 import CategoryPage from './../views/CategoryPage'
 import ProductPage from './../views/ProductPage'
 import HomePage from './../views/HomePage'
 import CartPage from './../views/CartPage'
+import AdminPage from '../views/admin/AdminPage'
+import LoginPage from './../views/admin/LoginPage'
+import ProductListPage from './../views/admin/ProductListPage'
+
+
+Vue.use(VueLocalStorage);
 
 Vue.use(VueRouter);
+
+function checkAuth(to, from, next) {
+    if (from.path === '/admin/login') {
+        // if coming from login page skip continue automatically to avoid loop
+        next();
+    }
+    Backendless.UserService.getCurrentUser().then(() => {
+        next();
+    }).catch(()=>{
+        next({ path: '/admin/login' });
+    });
+};
 
 const baseRoutes = [
     {
@@ -21,7 +40,19 @@ const baseRoutes = [
     {
         path: '/admin',
         component: AdminPage,
-        name: 'admin'
+        name: 'admin',
+        children: [
+            {
+                path: 'login',
+                component: LoginPage
+            },
+            {
+                path: 'products',
+                component: ProductListPage,
+                name: 'admin-products-list',
+                beforeEnter: checkAuth,
+            },
+        ]
     },
     {
         path: '/:catSlug',
@@ -47,5 +78,8 @@ export const VueRouterObject = {
 };
 
 const router = new VueRouter(VueRouterObject);
+
+
+
 
 export default router;
